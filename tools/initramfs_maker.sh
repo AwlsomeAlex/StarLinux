c5cb584a84f73fe8773823febd98bebc5c1048b5
AwlsomeAlex <awlsomeabc1@outlook.com>
2017-04-10 22:21:57 -0400

    Created InitramFS Maker and StarLinux Installer Script

---
+++ tools/initramfs_maker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##################################
+# StarLinux InitramFS Maker Tool #
+#--------------------------------#
+#Created by AwlsomeAlex {GPLv3}  #
+##################################
+# The main intention for this tool
+# is to use the precompiled linux
+# kernels and make an initramfs
+# in accordance to them and provide
+# a script which will help configure
+# SysLinux Bootloader.
---
+++ tools/install_starlinux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+############################
+# StarLinux Installer Tool #
+#--------------------------#
+#  Created by AwlsomeAlex  #
+############################
+# The main intention for this tool
+# is to use the precompiled linux
+# kernel and initramfs and use the
+# power of ArchLinux Live CD to 
+# fully install the StarLinux
+# Linux Distribution until an install
+# disk creator script can be installed.
