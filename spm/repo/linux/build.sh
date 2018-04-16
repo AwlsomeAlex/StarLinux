61907b71fabe126452e8bc954e48a108554f6681
AwlsomeAlex <AwlsomeAlex@users.noreply.github.com>
2018-04-16 19:54:52 -0400

    Added Build Info

--- spm/repo/linux/build.sh
+++ spm/repo/linux/build.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 ################################
 #------------------------------#
@@ -9,12 +10,19 @@
 
 . ../common.lib
 
+#---- Build Info ----#
+# Version Number: None
+# Last Build: None
+# Build Status: None
+# Identifier: AwlsomeAlex
+
 #---- Variables ----#
 DOWNLOAD_LINK="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.18.tar.xz"
-KERNEL="basic"
+KERNEL="basic" # There are three types of Kernels: 'mini' which only includes the bare bones functionality; 'basic' which includes SOME drivers like netwoking and FS; 'heavy' which includes MOST drivers and other optional features.
 PACKAGE="linux"
 ARCHIVE_FILE=${DOWNLOAD_LINK##*/}
 
+#---- Executable ----#
 depends Nebula
 message download 
 download $DOWNLOAD_LINK $SRC_DIR
