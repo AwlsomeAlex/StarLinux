03a249305e2b5af2ee5b9a1a45001cc1e038f5ce
AwlsomeAlex <awlsomeabc1@outlook.com>
2018-04-17 21:31:11 -0400

    Added Kernel Build

--- spm/repo/linux/build.sh
+++ spm/repo/linux/build.sh
@@ -19,6 +19,7 @@
 #---- Variables ----#
 DOWNLOAD_LINK="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.18.tar.xz"
 KERNEL="basic" # There are three types of Kernels: 'mini' which only includes the bare bones functionality; 'basic' which includes SOME drivers like netwoking and FS; 'heavy' which includes MOST drivers and other optional features.
+KERN_VER="4.14.18"
 ARCHIVE_FILE=${DOWNLOAD_LINK##*/}
 
 #---- Executable ----#
@@ -28,3 +29,12 @@ download $DOWNLOAD_LINK $SRC_DIR
 extract $SRC_DIR/$ARCHIVE_FILE linux
 message done "Downloaded and Extracted Linux Kernel."
 message .... "Building Linux Kernel...."
+cd $WORK_DIR/linux/linux-*
+mkdir -p $WORK_DIR/linux/linux_extra/lib/firmware
+cp $REPO_DIR/linux/mini.config $WORK_DIR/linux/linux-*/.config
+make \
+	CFLAGS="$CFLAGS" \
+	bzImage -j $NUM_JOBS
+cp arch/x86/boot/bzImage \
+	$FINAL_DIR/kernel-$KERN_VER.xz
+message done "Built Linux Kernel."
