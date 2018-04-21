5843d6d697083b026be84515274990517d6e1020
AwlsomeAlex <awlsomeabc1@outlook.com>
2018-04-21 17:39:20 -0400

    Will work on this later.

--- spm/repo/archive/build.sh
+++ spm/repo/archive/build.sh
@@ -18,8 +18,16 @@
 #---- Variables ----#
 KERN_VER="4.14.35" # !!!Temporary Versioning System!!! #
 STARINIT_DIR="$WORK_DIR/starinit/starinit"
+ARCHIVE_DIR="$FINAL_DIR/archive"
 
 #---- Executable ----#
 depends Nebula
 depends linux
 depends starinit
+echo -e "${RD} PACKAGE IS INCOMPLETE! PLEASE TRY AGAIN LATER!"
+exit 1
+#message .... "Packaging Current StarLinux Directory...."
+#mkdir $FINAL_DIR/archive
+#cd $STARINIT_DIR
+#find . | cpio -R root:root -H newc -o | xz -9 --check=none > ../initramfs-$KERN_VER.cpio.xz
+#cp ../initramfs-$KERN_VER.cpio.xz $ARCHIVE_DIR
