e04227edc93170694e9986e760765e68da34dc90
AwlsomeAlex <awlsomeabc1@outlook.com>
2018-04-29 16:22:54 -0400

    Changed StarINIT Location

--- spm/repo/starinit/build.sh
+++ spm/repo/starinit/build.sh
@@ -11,15 +11,15 @@
 
 #---- Build Info ----#
 # Version Number: GIT
-# Last Build: 4/19/18 10:11 EST
-# Build Status: PASS
+# Last Build: 4/29/18 4:24pm EST
+# Build Status: UNTESTED
 # Identifier: AwlsomeAlex
 
 #---- Variables ----#
-DOWNLOAD_LINK="https://github.com/AwlsomeAlex/starinit/archive/master.zip"
+DOWNLOAD_LINK="https://github.com/AwlsomeAlex/starinit/archive/old.zip"
 BUSYBOX_INSTALLED="$WORK_DIR/busybox/busybox_final"
 GLIBC_FINAL="$WORK_DIR/glibc/glibc_final"
-STARINIT="$WORK_DIR/starinit/starinit-master/src"
+STARINIT="$WORK_DIR/starinit/starinit-old/src"
 BUSYBOX_ARCH=$(file bin/busybox | cut -d' '  -f3)
 
 
@@ -32,7 +32,7 @@ message .... "Downloading and Extracting StarINIT...."
 download $DOWNLOAD_LINK $SRC_DIR
 unzip $SRC_DIR/master.zip -d $WORK_DIR/starinit/
 mkdir -p $WORK_DIR/starinit/starinit
-cp -r $WORK_DIR/starinit/starinit-master/src/* $WORK_DIR/starinit/starinit
+cp -r $WORK_DIR/starinit/starinit-old/src/* $WORK_DIR/starinit/starinit
 message done "Downloaded and Extracted StarINIT."
 echo -e "${GN}StarINIT has been downloaded.${NC}"
 # BUGGY CODING
