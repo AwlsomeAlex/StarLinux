a9bfb1c380ace1ad5126509cd126f4767fa158ea
AwlsomeAlex <awlsomeabc1@outlook.com>
2018-04-29 17:24:27 -0400

    Finished Implementing StarINIT 1.3.0, testing.

--- spm/repo/starinit-1.3.0/build.sh
+++ spm/repo/starinit-1.3.0/build.sh
@@ -12,30 +12,25 @@
 #---- Build Info ----#
 # Version Number: 1.3.0
 # Last Build: NONE
-# Build Status: BUILDING....
+# Build Status: TESTING....
 # Identifier: AwlsomeAlex
 
 #---- Variables ----#
 DOWNLOAD_LINK="https://github.com/AwlsomeAlex/starinit/archive/v1.3.0.tar.gz"
 STARINIT_VER="1.3.0"
 ARCHIVE_FILE=${DOWNLOAD_LINK##*/}
-BUSYBOX_INSTALLED="$WORK_DIR/busybox/busybox_final"
-GLIBC_FINAL="$WORK_DIR/glibc/glibc_final"
-STARINIT="$WORK_DIR/starinit/starinit-master/src"
-BUSYBOX_ARCH=$(file bin/busybox | cut -d' '  -f3)
 
 
 #---- Executable ----#
-#echo -e "${RD} PACKAGE IS INCOMPLETE! PLEASE TRY AGAIN LATER!${NC}"
-#depends Nebula
-#depends linux
-#depends glibc
-#depends busybox
+depends Nebula
 message .... "Downloading and Extracting StarINIT...."
 download $DOWNLOAD_LINK $SRC_DIR
 extract $SRC_DIR/$ARCHIVE_FILE starinit
-#mkdir -p $WORK_DIR/starinit/starinit
-#cp -r $WORK_DIR/starinit/starinit-master/src/* $WORK_DIR/starinit/starinit
-#message done "Downloaded and Extracted StarINIT."
-#echo -e "${GN}StarINIT has been downloaded.${NC}"
-# BUGGY CODING
+message done "Downloaded and Extracted StarINIT."
+message .... "Preparing StarINIT...."
+mkdir -p $WORK_DIR/starinit/starinit
+cd $WORK_DIR/starinit/starinit-*
+cp -r src/* $WORK_DIR/starinit/starinit
+message done "Prepared StarINIT."
+echo -e "${GN}StarINIT has been prepared.${NC}"
+
