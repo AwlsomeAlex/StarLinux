029ade61b5d1b29bbbd04d76cb71663483f8b6f7
AwlsomeAlex <awlsomeabc1@outlook.com>
2018-04-30 22:52:51 -0400

    Maybe?

--- spm/repo/starinit-1.3.0/build.sh
+++ spm/repo/starinit-1.3.0/build.sh
@@ -34,7 +34,8 @@ cd $WORK_DIR/starinit/starinit-*
 cp -r src/* $WORK_DIR/starinit/starinit
 message done "Prepared StarINIT."
 message .... "Configuring StarINIT...."
-sed -e 's/STARLINUX_BUILD=""/STARLINUX_BUILD="$STARLINUX_BUILD"/' $WORK_DIR/starinit/starinit/lib/starinit.lib $WORK_DIR/starinit/starinit/lib/starinit_tmp.lib
+sed -i '22s/.*/STARLINUX_BUILD=$(echo $STARLINUX_BUILD)/' $WORK_DIR/starinit/starinit/lib/starinit.lib
+#sed -e 's/STARLINUX_BUILD=""/STARLINUX_BUILD="$STARLINUX_BUILD"/' $WORK_DIR/starinit/starinit/lib/starinit.lib $WORK_DIR/starinit/starinit/lib/starinit_tmp.lib
 mv $WORK_DIR/starinit/starinit/lib/starinit_tmp.lib $WORK_DIR/starinit/starinit/lib/starinit.lib
 message done "Configured StarINIT."
 echo -e "${GN}StarINIT has been prepared.${NC}"
