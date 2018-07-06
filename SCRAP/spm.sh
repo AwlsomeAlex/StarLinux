4d197da94d7e248d8fbb6c45e6c1ee94b4cebfc4
AwlsomeAlex <awlsomealex@outlook.com>
2018-07-05 20:10:16 -0400

    Started Fresh

---
+++ SCRAP/spm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+########################
+#----------------------#
+# Star Package Manager #
+#----------------------#
+########################
+# Created by AwlsomeAlex [GNU GPLv3]
+# Protostar Branch
+
+#############
+# Variables #
+#############
+REPO_DIR="/tmp/spm/repo" # ONLY VARIABLE NOT DEFINED BY CONFIG!
+RD='\033[1;31m'
+GN='\033[1;32m'
+NC='\033[0m'
+COMMAND="$1"
+PACKAGE="$2"
+
+
+##################
+# Child Function #
+##################
+function download_repo() {
+	if [ -d $REPO_DIR ]; then
+		message WARN "The Repository is already downloaded! Continuing..."
+	else
+		echo -e "${GN}StarLinux Package Manager - Downloading Repository ${NC}"
+		mkdir -p $REPO_DIR
+		cd $REPO_DIR/..
+		wget https://github.com/AwlsomeAlex/StarLinux/archive/protostar.zip -q --show-progress
+		unzip -q protostar.zip
+		rm -rf protostar.zip
+		cp -r StarLinux-protostar/repo/* repo/
+		rm -rf StarLinux-protostar
+	fi
+}
+
+
+if [ ! -d $REPO_DIR ]; then
+	download_repo
+fi
+. $REPO_DIR/common.lib
+run_lib
+
